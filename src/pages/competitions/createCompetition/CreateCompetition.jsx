c2879b10557353e09b550b3d0bb41dc6024ca457
Erik Somnäs <erik.somnas@gmail.com>
2023-03-28 13:24:08 +0200

    createCompetition

--- src/pages/competitions/createCompetition/CreateCompetition.jsx
+++ src/pages/competitions/createCompetition/CreateCompetition.jsx
@@ -1,14 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import DashboardLayout from 'layouts/containers/DashboardLayout';
 import DashboardNavbar from 'layouts/components/navbars/dashboardNavbar/DashboardNavbar';
 import Footer from 'layouts/components/Footer';
 
+// @mui material components
+import Grid from '@mui/material/Grid';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Card from '@mui/material/Card';
+
+// Material Dashboard 2 PRO React components
+import MDBox from 'components/MDBox';
+import MDButton from 'components/MDButton';
+import MDTypography from 'components/MDTypography';
+
+// NewProduct page components
+import ProductInfo from './components/ProductInfo';
+import Media from './components/Media';
+import Socials from './components/Socials';
+import Pricing from './components/Pricing';
+
+function getSteps() {
+    return ['1. Product Info', '2. Media', '3. Social', '4. Pricing'];
+}
+
+function getStepContent(stepIndex) {
+    switch (stepIndex) {
+        case 0:
+            return <ProductInfo />;
+        case 1:
+            return <Media />;
+        case 2:
+            return <Socials />;
+        case 3:
+            return <Pricing />;
+        default:
+            return null;
+    }
+}
+
 export default function CreateCompetition() {
+
+    const [activeStep, setActiveStep] = useState(0);
+    const steps = getSteps();
+    const isLastStep = activeStep === steps.length - 1;
+
+    const handleNext = () => setActiveStep(activeStep + 1);
+    const handleBack = () => setActiveStep(activeStep - 1);
+
     return (
         <DashboardLayout>
             <DashboardNavbar title='Create Competition' />
-            <div>CreateCompetition</div>
+            <MDBox mt={5} mb={9}>
+                <Grid container justifyContent='center'>
+                    <Grid item xs={12} lg={8}>
+                        <MDBox mt={6} mb={8} textAlign='center'>
+                            <MDBox mb={1}>
+                                <MDTypography variant='h3' fontWeight='bold'>
+                                    Add New Product
+                                </MDTypography>
+                            </MDBox>
+                            <MDTypography variant='h5' fontWeight='regular' color='secondary'>
+                                This information will describe more about the product.
+                            </MDTypography>
+                        </MDBox>
+                        <Card>
+                            <MDBox mt={-3} mb={3} mx={2}>
+                                <Stepper activeStep={activeStep} alternativeLabel>
+                                    {steps.map((label) => (
+                                        <Step key={label}>
+                                            <StepLabel>{label}</StepLabel>
+                                        </Step>
+                                    ))}
+                                </Stepper>
+                            </MDBox>
+                            <MDBox p={2}>
+                                <MDBox>
+                                    {getStepContent(activeStep)}
+                                    <MDBox mt={3} width='100%' display='flex' justifyContent='space-between'>
+                                        {activeStep === 0 ? (
+                                            <MDBox />
+                                        ) : (
+                                            <MDButton variant='gradient' color='light' onClick={handleBack}>
+                                                back
+                                            </MDButton>
+                                        )}
+                                        <MDButton
+                                            variant='gradient'
+                                            color='dark'
+                                            onClick={!isLastStep ? handleNext : undefined}
+                                        >
+                                            {isLastStep ? 'send' : 'next'}
+                                        </MDButton>
+                                    </MDBox>
+                                </MDBox>
+                            </MDBox>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </MDBox>
             <Footer />
         </DashboardLayout>
     );
