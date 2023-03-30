2e54f820b536eb7efad80517967105f8343af9c5
Erik Somnäs <erik.somnas@gmail.com>
2023-03-30 09:23:52 +0200

    editCompetition layout

--- src/pages/competitions/editCompetition/EditCompetition.jsx
+++ src/pages/competitions/editCompetition/EditCompetition.jsx
@@ -17,6 +17,7 @@ import ProductImage from './components/ProductImage';
 import ProductInfo from './components/ProductInfo';
 import Socials from './components/Socials';
 import Pricing from './components/Pricing';
+import Sidenav from './components/Sidenav';
 
 export default function EditCompetition() {
 
@@ -25,45 +26,54 @@ export default function EditCompetition() {
     return (
         <DashboardLayout>
             <DashboardNavbar title={competitionName} />
-            <MDBox my={3}>
-                <MDBox mb={6}>
-                    <Grid container spacing={3} alignItems='center'>
-                        <Grid item xs={12} lg={6}>
-                            <MDTypography variant='h4' fontWeight='medium'>
-                                Make the changes below
-                            </MDTypography>
-                            <MDBox mt={1} mb={2}>
-                                <MDTypography variant='body2' color='text'>
-                                    We’re constantly trying to express ourselves and actualize our dreams. If you have
-                                    the opportunity to play.
-                                </MDTypography>
-                            </MDBox>
-                        </Grid>
-                        <Grid item xs={12} lg={6}>
-                            <MDBox display='flex' justifyContent='flex-end'>
-                                <MDButton variant='gradient' color='info'>
-                                    save
-                                </MDButton>
-                            </MDBox>
-                        </Grid>
-                    </Grid>
-                </MDBox>
+            <MDBox mt={4}>
                 <Grid container spacing={3}>
-                    <Grid item xs={12} lg={4}>
-                        <ProductImage />
+                    <Grid item xs={12} lg={3}>
+                        <Sidenav />
                     </Grid>
-                    <Grid item xs={12} lg={8}>
-                        <ProductInfo />
-                    </Grid>
-                    <Grid item xs={12} lg={4}>
-                        <Socials />
-                    </Grid>
-                    <Grid item xs={12} lg={8}>
-                        <Pricing />
+                    <Grid item xs={12} lg={9}>
+                        <MDBox mb={3}>
+                            <MDBox mb={6}>
+                                <Grid container spacing={3} alignItems='center'>
+                                    <Grid item xs={12} lg={6} >
+                                        <MDTypography variant='h4' fontWeight='medium'>
+                                            Make the changes below
+                                        </MDTypography>
+                                        <MDBox mt={1} mb={2}>
+                                            <MDTypography variant='body2' color='text'>
+                                                We’re constantly trying to express ourselves and actualize our dreams. If you have
+                                                the opportunity to play.
+                                            </MDTypography>
+                                        </MDBox>
+                                    </Grid>
+                                    <Grid item xs={12} lg={6}>
+                                        <MDBox display='flex' justifyContent='flex-end'>
+                                            <MDButton variant='gradient' color='info'>
+                                                save
+                                            </MDButton>
+                                        </MDBox>
+                                    </Grid>
+                                </Grid>
+                            </MDBox>
+                            <Grid container spacing={3}>
+                                <Grid item xs={12}>
+                                    <ProductImage />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <ProductInfo />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Socials />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Pricing />
+                                </Grid>
+                            </Grid>
+                        </MDBox>
                     </Grid>
                 </Grid>
             </MDBox>
-            <Footer />
-        </DashboardLayout>
+            < Footer />
+        </DashboardLayout >
     );
 }
